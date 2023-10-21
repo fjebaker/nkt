5cb95441be7e66c4b18c243e211f91af60ed7085
fjebaker <fergusbkr@gmail.com>
2023-10-22 00:07:19 +0100

    feat: printer interface

--- src/commands/read.zig
+++ src/commands/read.zig
@@ -29,14 +29,14 @@ pub const extended_help =
 const Selection = cli.selections.Selection;
 const ContainerSelection = cli.selections.ContainerSelection;
 
-selection: Selection,
+selection: ?Selection,
 where: ?ContainerSelection,
 number: usize,
 all: bool,
 
 pub fn init(itt: *cli.ArgIterator) !Self {
     var self: Self = .{
-        .selection = Selection.today(),
+        .selection = null,
         .where = null,
         .number = 25,
         .all = false,
@@ -72,43 +72,218 @@ pub fn init(itt: *cli.ArgIterator) !Self {
     return self;
 }
 
+const NoSuchCollection = State.Collection.Errors.NoSuchCollection;
 pub fn run(
     self: *Self,
     state: *State,
     out_writer: anytype,
 ) !void {
-    const collection = cli.selections.find(state, self.where, self.selection) orelse
-        return State.Collection.Errors.NoSuchCollection;
+    const N = if (self.all) null else self.number;
+    var printer = Printer.init(state.allocator, N);
+    defer printer.deinit();
 
-    switch (collection) {
-        .JournalEntry => |journal_entry| {
-            try self.readJournalEntry(state.allocator, journal_entry.item, out_writer);
-        },
-        .NoteWithJournalEntry => |both| {
-            try self.readJournalEntry(state.allocator, both.journal.item, out_writer);
+    if (self.selection) |selection| {
+        // if selection is specified
+        const collection = cli.selections.find(state, self.where, selection) orelse
+            return NoSuchCollection;
+
+        switch (collection) {
+            .JournalEntry => |journal_entry| {
+                try self.readJournalEntry(journal_entry.item, &printer);
+            },
+            .NoteWithJournalEntry => |both| {
+                try self.readJournalEntry(both.journal.item, &printer);
+            },
+            else => {},
+        }
+    } else if (self.where) |w| switch (w.container) {
+        // if no selection, but a collection
+        .Journal => {
+            const journal = state.getJournal(w.name) orelse
+                return NoSuchCollection;
+            try self.readJournal(journal, &printer);
         },
         else => {},
+    } else {
+        // default behaviour
+        const journal = state.getJournal("diary").?;
+        try self.readJournal(journal, &printer);
     }
+
+    try printer.drain(out_writer);
 }
 
-const Journal = Topology.Journal;
-fn readJournalEntry(
+fn readJournal(
     self: *Self,
-    allocator: std.mem.Allocator,
-    entry: *Journal.Entry,
-    writer: anytype,
+    journal: *State.TrackedJournal,
+    printer: *Printer,
 ) !void {
-    try writer.print("JournalEntry {s}\n", .{entry.name});
+    var alloc = printer.mem.allocator();
+    var entry_list = try journal.getDatedEntryList(alloc);
 
-    for (entry.items) |item| {
-        const date = utils.Date.initUnixMs(item.created);
-        const time_of_day = try utils.formatTimeBuf(date);
-        try writer.print("{s} - {s}\n", .{ time_of_day, item.item });
+    entry_list.sortBy(.Created);
+    entry_list.reverse();
+
+    var line_count: usize = 0;
+    const last = for (0.., entry_list.items) |i, item| {
+        const N = item.entry.items.len;
+        line_count += N;
+        if (!printer.couldFit(line_count)) {
+            break i;
+        }
+    } else entry_list.items.len - 1;
+
+    printer.reverse();
+    for (entry_list.items[0 .. last + 1]) |entry| {
+        try self.readJournalEntry(entry.entry, printer);
+        if (!printer.couldFit(1)) break;
+    }
+    printer.reverse();
+}
+
+fn printEntryItem(writer: Printer.Writer, item: Topology.Journal.Entry.Item) Printer.WriteError!void {
+    const date = utils.Date.initUnixMs(item.created);
+    const time_of_day = utils.formatTimeBuf(date) catch return Printer.WriteError.DateError;
+    try writer.print("{s} - {s}\n", .{ time_of_day, item.item });
+}
+
+fn readJournalEntry(
+    _: *Self,
+    entry: *const Topology.Journal.Entry,
+    printer: *Printer,
+) !void {
+    try printer.addHeading("Journal entry: {s}\n\n", .{entry.name});
+    _ = try printer.addItems(entry.items, printEntryItem);
+}
+
+const Printer = struct {
+    const StringList = std.ArrayList(u8);
+
+    fn ChildType(comptime T: anytype) type {
+        return @typeInfo(T).Pointer.child;
     }
 
-    _ = self;
-    _ = allocator;
-}
+    pub const Writer = StringList.Writer;
+    pub const WriteError = error{ OutOfMemory, DateError };
+    pub const PrinterError = error{HeadingMissing};
+
+    const Chunk = struct {
+        heading: []const u8,
+        lines: StringList,
+
+        fn print(self: *Chunk, writer: anytype) !void {
+            _ = try writer.writeAll(self.heading);
+            _ = try writer.writeAll(try self.lines.toOwnedSlice());
+        }
+    };
+
+    const ChunkList = std.ArrayList(Chunk);
+
+    remaining: ?usize,
+    mem: std.heap.ArenaAllocator,
+    chunks: ChunkList,
+    current: ?*Chunk = null,
+
+    pub fn init(alloc: std.mem.Allocator, N: ?usize) Printer {
+        var mem = std.heap.ArenaAllocator.init(alloc);
+        errdefer mem.deinit();
+
+        var list = ChunkList.init(alloc);
+
+        return .{ .remaining = N, .chunks = list, .mem = mem };
+    }
+
+    pub fn drain(self: *const Printer, writer: anytype) !void {
+        var chunks = self.chunks.items;
+        // print first chunk
+        if (chunks.len > 0) {
+            try chunks[0].print(writer);
+        }
+        if (chunks.len > 1) {
+            for (chunks[1..]) |*chunk| {
+                _ = try writer.writeAll("\n");
+                try chunk.print(writer);
+            }
+        }
+    }
+
+    fn subRemainder(self: *Printer, i: usize) bool {
+        if (self.remaining == null) return true;
+        self.remaining.? -= i;
+        return self.remaining.? != 0;
+    }
+
+    fn allowMore(self: *const Printer) bool {
+        if (self.remaining) |rem| {
+            return rem > 0;
+        }
+        return true;
+    }
+
+    pub fn reverse(self: *Printer) void {
+        std.mem.reverse(Chunk, self.chunks.items);
+    }
+
+    pub fn addItems(
+        self: *Printer,
+        items: anytype,
+        comptime write_function: fn (writer: Writer, item: ChildType(@TypeOf(items))) WriteError!void,
+    ) !bool {
+        var chunk = self.current orelse return PrinterError.HeadingMissing;
+
+        var writer = chunk.lines.writer();
+
+        const start = if (self.remaining) |rem|
+            (items.len -| rem)
+        else
+            0;
+
+        for (items[start..]) |item| {
+            try write_function(writer, item);
+        }
+
+        return self.subRemainder(items.len - start);
+    }
+
+    pub fn addLine(self: *Printer, comptime format: []const u8, args: anytype) !bool {
+        var chunk = self.current orelse return PrinterError.HeadingMissing;
+
+        if (self.allowMore()) {
+            chunk.lines.writer().print(format, args);
+        }
+        return self.subRemainder(1);
+    }
+
+    pub fn addHeading(self: *Printer, comptime format: []const u8, args: anytype) !void {
+        var alloc = self.mem.allocator();
+
+        var heading_writer = StringList.init(alloc);
+        var lines = StringList.init(alloc);
+
+        try heading_writer.writer().print(format, args);
+
+        var chunk: Chunk = .{
+            .heading = try heading_writer.toOwnedSlice(),
+            .lines = lines,
+        };
+        try self.chunks.append(chunk);
+
+        self.current = &self.chunks.items[self.chunks.items.len - 1];
+    }
+
+    pub fn deinit(self: *Printer) void {
+        self.chunks.deinit();
+        self.mem.deinit();
+        self.* = undefined;
+    }
+
+    pub fn couldFit(self: *Printer, size: usize) bool {
+        if (self.remaining) |rem| {
+            return rem >= size;
+        }
+        return true;
+    }
+};
 
 // fn readDiary(
 //     entry: notes.diary.Entry,
