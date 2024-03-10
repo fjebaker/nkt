abbcfb96e3bfd4dc43652ae8b827a1f55f8173c0
fjebaker <fergusbkr@gmail.com>
2024-03-10 01:59:36 +0000

    build: skip install test artifact

--- build.zig
+++ build.zig
@@ -57,5 +57,5 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 
-    b.installArtifact(unit_tests);
+    // b.installArtifact(unit_tests);
 }
