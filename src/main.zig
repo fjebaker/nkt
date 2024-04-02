6be794f3cf691b32ddb2d7bfc01f148c516738f8
fjebaker <fergusbkr@gmail.com>
2024-04-02 11:26:57 +0100

    test: added more end-to-end coverage

--- src/main.zig
+++ src/main.zig
@@ -190,5 +190,22 @@ test "end-to-end" {
 
     // basic commands work
     try testExecute(state, &.{"help"});
+    try testExecute(state, &.{"init"});
     try testExecute(state, &.{"config"});
+    try testExecute(state, &.{ "log", "hello world" });
+    try testExecute(state, &.{ "new", "tag", "abc" });
+
+    // inline tags
+    try testExecute(state, &.{ "log", "hello world @abc" });
+
+    // seperate tags
+    try testExecute(state, &.{ "log", "hello", "@abc" });
+
+    // task creation
+    try testExecute(state, &.{ "task", "do something", "--due", "monday" });
+    try testExecute(state, &.{ "task", "do something", "soon", "--due", "monday" });
+
+    // retrieval
+    try testExecute(state, &.{ "ls", "--tasklist", "todo" });
+    try testExecute(state, &.{"read"});
 }
