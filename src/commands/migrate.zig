5700c5dc81a3387e6f2bab7b1565536dab3808eb
fjebaker <fergusbkr@gmail.com>
2024-04-08 00:28:10 +0100

    chore: remove migrations

--- src/commands/migrate.zig
+++ src/commands/migrate.zig
@@ -5,63 +5,46 @@ const utils = @import("../utils.zig");
 const commands = @import("../commands.zig");
 const Root = @import("../topology/Root.zig");
 
-const migration = @import("../migration/migrate.zig");
-
 const Self = @This();
 
 pub const short_help = "Migrate differing versions of nkt's topology";
 pub const long_help = short_help;
-pub const arguments = cli.Arguments(&.{
-    .{
-        .arg = "touch",
-        .help = "Touch all files",
-    },
-});
-
-touch: bool,
+pub const arguments = cli.Arguments(&.{});
 
 pub fn fromArgs(_: std.mem.Allocator, itt: *cli.ArgIterator) !Self {
     const args = try arguments.parseAll(itt);
-    return .{ .touch = args.touch != null };
+    _ = args;
+    return .{};
 }
 
 pub fn execute(
-    self: *Self,
-    allocator: std.mem.Allocator,
+    _: *Self,
+    _: std.mem.Allocator,
     root: *Root,
-    writer: anytype,
+    _: anytype,
     _: commands.Options,
 ) !void {
-    if (self.touch) {
+    // load and write all files
+    try root.load();
+    for (root.info.journals) |jrnl| {
+        var journal = (try root.getJournal(jrnl.name)).?;
+        for (journal.info.days) |day| {
+            const d = journal.getDay(day.name).?;
+            _ = try journal.getEntries(d);
+        }
+        try journal.writeDays();
 
-        // load and write all files
-        try root.load();
-        for (root.info.journals) |jrnl| {
-            var journal = (try root.getJournal(jrnl.name)).?;
-            for (journal.info.days) |day| {
-                const d = journal.getDay(day.name).?;
-                _ = try journal.getEntries(d);
-            }
-            try journal.writeDays();
-
-            root.markModified(jrnl, .CollectionJournal);
-        }
-        for (root.info.directories) |dir| {
-            _ = (try root.getDirectory(dir.name)).?;
-            root.markModified(dir, .CollectionDirectory);
-        }
-        for (root.info.tasklists) |tl| {
-            _ = (try root.getTasklist(tl.name)).?;
-            root.markModified(tl, .CollectionTasklist);
-        }
-        try root.writeChanges();
-        try root.writeTags();
-        try root.writeChains();
-    } else {
-        try migration.migratePath(
-            allocator,
-            root.fs.?.root_path,
-        );
-        try writer.writeAll("Migration complete\n");
+        root.markModified(jrnl, .CollectionJournal);
     }
+    for (root.info.directories) |dir| {
+        _ = (try root.getDirectory(dir.name)).?;
+        root.markModified(dir, .CollectionDirectory);
+    }
+    for (root.info.tasklists) |tl| {
+        _ = (try root.getTasklist(tl.name)).?;
+        root.markModified(tl, .CollectionTasklist);
+    }
+    try root.writeChanges();
+    try root.writeTags();
+    try root.writeChains();
 }
