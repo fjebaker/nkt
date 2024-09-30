6b5bbf7fd3e79eec4138e565740a06cb91a13365
fjebaker <fergusbkr@gmail.com>
2024-09-30 18:15:59 +0100

    chore: bump version 0.2.7

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 const VERSION: std.SemanticVersion = .{
     .major = 0,
     .minor = 2,
-    .patch = 6,
+    .patch = 7,
 };
 
 pub fn addTracy(b: *std.Build, step: *std.Build.Step.Compile) !void {
